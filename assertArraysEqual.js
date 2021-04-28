e727057f34927e03b7f7b69434651670844fcc34
mandatoryrecess <rossanneclamp@gmail.com>
2021-04-28 04:48:07 +0000

    Compare two arrays and find if they are equal

---
+++ assertArraysEqual.js
@@ -0,0 +1,15 @@
+const  assertArraysEqual = function(arr1, arr2) {
+  if (arr1.length !== arr2.length)
+    return console.log(`💣 💣 Array Length is not the same: ${arr1}!== ${arr2}`);
+   
+  for (let i = 0; i < arr1.length; i++)
+    if (arr1[i] !== arr2[i])
+    
+      return console.log(`💣 💣 Array Order is not the same: ${arr1}!== ${arr2}`);
+  
+  return console.log(`🌈 🏆 Looks the Same!: ${arr1} === ${arr2}`);
+  
+};
+
+
+assertArraysEqual([1, 2, 3], [1, 2, 3]);
