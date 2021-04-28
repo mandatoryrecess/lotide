a3099e5c191f7b62951344c9aaacc7d1b026d939
Ross Clamp <81119910+mandatoryrecess@users.noreply.github.com>
2021-04-27 19:02:26 -0700

    Add files via upload

---
+++ eqArrays.js
@@ -0,0 +1,21 @@
+const assertEqual = function(actual, expected) {
+  if (actual === expected) {
+    console.log(`🌈 🏆 Assertion Passed:${actual}===${expected}`);
+  } else {
+    console.log(`💣 💣 Assertion Failed: ${actual}!==+${expected}`);
+  }
+};
+
+const eqArrays = function(arr1, arr2) {
+  if (arr1.length !== arr2.length) {
+    return false;
+  }
+  for (let i = 0; i < arr1.length; i++) {
+    if (arr1[i] !== arr2[i]) {
+      return false;
+    }
+  }
+  return true;
+};
+
+assertEqual(eqArrays([1, 2, 3], [1, 2, 3]), true); // => should PASS
