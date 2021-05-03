e20f3844dd30e70b5dd8b23c4a10639b408c397e
mandatoryrecess <rossanneclamp@gmail.com>
2021-05-03 23:51:29 +0000

    refactored

--- eqArrays.js
+++ eqArrays.js
@@ -1,11 +1,3 @@
-const assertEqual = function(actual, expected) {
-  if (actual === expected) {
-    console.log(`🌈 🏆 Assertion Passed:${actual}===${expected}`);
-  } else {
-    console.log(`💣 💣 Assertion Failed: ${actual}!==+${expected}`);
-  }
-};
-
 const eqArrays = function(arr1, arr2) {
   if (arr1.length !== arr2.length) {
     return false;
@@ -18,4 +10,6 @@ const eqArrays = function(arr1, arr2) {
   return true;
 };
 
-assertEqual(eqArrays([1, 2, 3], [1, 2, 3]), true); // => should PASS
+
+
+module.exports = eqArrays;
--- tail.js
+++ tail.js
@@ -1,17 +1,9 @@
-const assertEqual = function(actual, expected) {
-  if (actual === expected) {
-    console.log(`🌈 🏆 Assertion Passed:${actual}===${expected}`);
-  } else {
-    console.log(`💣 💣 Assertion Failed: ${actual}!==+${expected}`);
-  }
-};
-
-const words = ["Yo Yo", "Lighthouse", "Labs"];
 
 const tail = function(words) {
   let tailArray = [];
   tailArray = words.slice(1);
   return tailArray;
 };
-tail(words);
-assertEqual(words.length, 3);
+
+
+module.exports = tail;
