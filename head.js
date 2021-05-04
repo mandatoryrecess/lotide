468cea384fddc496b4f9dc1cf01f5190ebef11f1
mandatoryrecess <rossanneclamp@gmail.com>
2021-05-04 03:37:14 +0000

    added chai

--- head.js
+++ head.js
@@ -1,9 +1,11 @@
-const assertEqual = require('./assertEqual');
+// const assertEqual = require('./assertEqual');
 
 const head = function(array) {
   return array[0];
 };
 
+console.log(head([5,6,7]));
 
-assertEqual(head([5,6,7]), 5);
-assertEqual(head(["Hello", "Lighthouse", "Labs"]), "Hello");
+// console.log(head([1, 2, 3]))
+
+module.exports = head
