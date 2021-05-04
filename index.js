2d27c480380c95baaff1ef01244b21fd1b39c118
mandatoryrecess <rossanneclamp@gmail.com>
2021-05-04 04:49:27 +0000

    New ES5 syntax

--- index.js
+++ index.js
@@ -1,9 +1,33 @@
+const assertArraysEqual   = require('./assertArraysEqual');
+const assertEqual = require('./assertEqual');
+const assertObjectsEqual = require('./assertObjectsEqual');
+const countLetters = require('./countLetters');
+const countOnly = require('./countOnly');
+const eqArrays   = require('./eqArrays');
+const findKeyByValue   = require('./findKeyByValue');
+const findKey = require('./findKey');
 const head   = require('./head');
+const letterPositions = require('./letterPositions');
 const tail   = require('./tail');
+const map = require('./map');
 const middle = require('./middle');
+const takeUntil = require('./takeUntil');
+const  without = require('./without');
 
 module.exports = {
-  head:   head,
-  tail:   tail,
-  middle: middle
+  assertArraysEqual,
+  assertEqual,
+  assertObjectsEqual,
+  countLetters, 
+  countOnly,
+  eqArrays, 
+  findKeyByValue,
+  findKey, 
+  head,
+  letterPositions, 
+  map,
+  middle,
+  tail,
+  takeUntil,
+  without
 };
